5bffbd698339f22937f6a6a033f8ecb073af854c
DESKTOP-DFK0E9D\seba <dicoud@gmail.com>
2024-01-03 13:17:53 +0100

    Added games model and viewModel

---
+++ GameStore/Models/Games.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GameStore.Models
+{
+    public class Games
+    {
+        public string ID { get; set; }
+        public string? Name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+
+        public Games()
+        {
+            this.ID = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ GameStore/ViewModels/GameViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.ViewModels
+{
+    public class GameViewModel
+    {
+        public string? Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
