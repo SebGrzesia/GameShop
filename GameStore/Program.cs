7c641ce306d002b37d837babd7b2fd7e4d796229
DESKTOP-DFK0E9D\seba <dicoud@gmail.com>
2024-01-03 13:28:29 +0100

    Scaffolded Game store model

--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GameStore.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<GameStoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GameStoreContext") ?? throw new InvalidOperationException("Connection string 'GameStoreContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
