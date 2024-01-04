4c3005a421c0c2d4d14d631861c294cb30333764
DESKTOP-DFK0E9D\seba <dicoud@gmail.com>
2024-01-04 16:00:57 +0100

    Done

--- GameStore/ViewModels/Games/DetailsGameViewModel.cs
+++ GameStore/ViewModels/Games/DetailsGameViewModel.cs
@@ -1,11 +1,17 @@
-﻿namespace GameStore.ViewModels.Games
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.ViewModels.Games
 {
     public class DetailsGameViewModel
     {
         public string ID { get; set; }
         public string? Name { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
--- GameStore/ViewModels/Games/GamesViewModel.cs
+++ GameStore/ViewModels/Games/GamesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameStore.ViewModels.Games
 {
@@ -6,9 +7,11 @@ namespace GameStore.ViewModels.Games
     {
         public string ID { get; set; }
         public string? Name { get; set; }
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
